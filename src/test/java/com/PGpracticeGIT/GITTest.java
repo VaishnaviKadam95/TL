e66c968bfcbc59dc8b1c202ede2f929ff899e0ff
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2023-12-03 18:52:06 +0530

    Framework is done

---
+++ src/test/java/com/PGpracticeGIT/GITTest.java
@@ -0,0 +1,11 @@
+package com.PGpracticeGIT;
+
+public class GITTest {
+public void Focus() {
+	System.out.println("focus on what you are doing regardless of everything ");
+}
+
+public void Belive() {
+	System.out.println("Belive on yourself and god");
+}
+}
